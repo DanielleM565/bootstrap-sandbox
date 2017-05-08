0304395bab53ce3207690fb594b29530084257f1
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-05-08 10:33:23 -0600

    added basic html page

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Bootstrap Lecture Demo</title>
+	</head>
+	<body>
+		<h2>This is a bootstrap page.</h2>
+	</body>
+</html>
